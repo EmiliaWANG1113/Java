578184f206dd53f3b47343f58e516676642c7cd7
Emilia <evelyn8611@gmail.com>
2023-06-13 23:25:39 +0800

    Add files via upload

---
+++ Practice/practice1/TestDemo.java
@@ -0,0 +1,16 @@
+package codegym;
+
+public class TestDemo {
+	
+	// 屬性定義
+	public static String gender;
+	
+	public static void main(String args[]) {
+		String name="Amy";
+		gender="girl";
+		int age=21;
+		System.out.println("My name is " + name + ", I'm " + age + " years old, I'm " + gender);
+	}
+
+}
+
