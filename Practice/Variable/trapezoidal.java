28b964ac51ef229854fdd4313842593414303771
Emilia <evelyn8611@gmail.com>
2023-06-14 10:15:53 +0800

    Add files via upload

---
+++ Practice/Variable/trapezoidal.java
@@ -0,0 +1,17 @@
+package codegym;
+
+public class trapezoidal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int up = 4;
+		int donw = 6;
+		int height = 5;
+		
+		int result = (up+donw)*height/2; 
+		
+		System.out.println(result);
+
+	}
+
+}
