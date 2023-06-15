6c9cc9b948d5046b343fcfdb09e250f32c0d20ea
Emilia <evelyn8611@gmail.com>
2023-06-15 19:17:38 +0800

    Add files via upload

---
+++ Practice/Array/array.java
@@ -0,0 +1,14 @@
+package codegym;
+
+public class array {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int[]x[] = {{1,2},{3,4,5},{6,7,8,9}};
+		int[][]y = x;
+		System.out.print(y[2][1]);
+
+	}
+
+}
